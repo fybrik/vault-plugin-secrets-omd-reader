341bd385bcfbb8873743ee48e4d34093bc4ad858
Doron Chen <cdoron@il.ibm.com>
2022-10-11 12:06:23 +0300

    explain nolint in main.go
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- pkg/cmd/vault-plugin-secrets-omd-reader/main.go
+++ pkg/cmd/vault-plugin-secrets-omd-reader/main.go
@@ -21,7 +21,7 @@ func main() {
 	// Please see https://www.hashicorp.com/blog/building-a-vault-secure-plugin for more info.
 	apiClientMeta := &api.PluginAPIClientMeta{}
 	flags := apiClientMeta.FlagSet()
-	err := flags.Parse(os.Args[1:]) //nolint
+	err := flags.Parse(os.Args[1:]) //nolint //temporarily needed due to golangci-lint bug: https://github.com/golangci/golangci-lint/discussions/3286
 	if err != nil {
 		logger := hclog.New(&hclog.LoggerOptions{})
 
