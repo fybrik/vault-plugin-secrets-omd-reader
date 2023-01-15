7e29e19a2ff9e68d483d1b7d569525a446985cd6
Doron Chen <cdoron@il.ibm.com>
2023-01-15 18:27:47 +0200

    phase 1: refactoring
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- pkg/omsecrets/openmetadata_secrets_reader.go
+++ pkg/omsecrets/openmetadata_secrets_reader.go
@@ -5,10 +5,12 @@ package omsecrets
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/go-hclog"
 
 	omclient "fybrik.io/vault-plugin-secrets-omd-reader/pkg/openmetadata"
+	dbtypes "fybrik.io/vault-plugin-secrets-omd-reader/pkg/openmetadata/database-types"
 )
 
 type OpenMetadataSecretsReader struct {
@@ -17,10 +19,19 @@ type OpenMetadataSecretsReader struct {
 
 // GetSecret returns the content of openmetadata secret.
 func (s *OpenMetadataSecretsReader) GetSecret(ctx context.Context, secretName string, log hclog.Logger) (map[string]interface{}, error) {
+	nameToDatabaseStruct := map[string]dbtypes.Databasetype{
+		"Datalake": dbtypes.S3{},
+	}
+
 	databaseService, err := s.client.GetConnectionInformation(ctx, secretName, log)
 	if err != nil {
 		return nil, err
 	}
+	serviceType := databaseService.GetServiceType()
+	dt, found := nameToDatabaseStruct[serviceType]
+	if !found {
+		return nil, fmt.Errorf("Service type %s not recognized", serviceType)
+	}
 	config := databaseService.Connection.GetConfig()
-	return s.client.ExtractSecretsFromConfig(config)
+	return dt.ExtractSecretsFromConfig(config)
 }
